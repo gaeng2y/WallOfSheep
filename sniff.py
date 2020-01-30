860f85f721c78081069f463184f4b4265fc69795
Yang Kyeong Mo <gaeng1107@gmail.com>
2020-01-30 19:53:10 +0900

    modi all

--- sniff.py
+++ sniff.py
@@ -41,8 +41,8 @@ def sniff():
                         src_port, dst_port, *sth = u_tcp(pkt[34:54])
                         if src_port is 80 or dst_port is 80:                        
                             #print("http packet")
-                            return pkt[54:], ip2str(src_ip)
-                            #print(pkt[54:])
+                            #return pkt[54:], ip2str(src_ip)
+                            print(pkt[54:])
                             #parsePkt(pkt[54:])
 
                 except KeyError:                
