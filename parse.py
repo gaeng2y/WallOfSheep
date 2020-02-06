480a54469ff0e2fe622f2bfa9081f4798c15202c
Yang Kyeong Mo <gaeng1107@gmail.com>
2020-02-06 19:11:53 +0900

    Change codes

--- parse.py
+++ parse.py
@@ -4,25 +4,19 @@ import pymysql
 import xml.etree.ElementTree as ET
 import sniff
 
-METHOD = re.compile(r"(POST|GET)")
-HOST = re.compile(r"host\s?:\s?(?P<host> .*)", re.I)
-CONTYPE = re.compile(r"content-type\s?:\s?(?P<contenttype> .*)", re.I)
+METHOD = re.compile(rb"(POST|GET)")
+HOST = re.compile(rb"host\s?:\s?(?P<host> .*)", re.I)
+CONTYPE = re.compile(rb"content-type\s?:\s?(?P<contenttype> .*)", re.I)
 #USERNAME = re.compile(r"(userId|m_id|id)[^(&|=)]*=(?P<username>[^(&|=)]*)", re.I)
-USERNAME = re.compile(r"(userId|login|m_id|id)=(?P<username>[^(&|=)]*)", re.I)
-PASSWD = re.compile(r"(pass|userPw|pw)[^(&|=)]*=(?P<pass>[^(&|=|)]*)", re.I)
+USERNAME = re.compile(rb"(userId|login|m_id|id)=(?P<username>[^(&|=)]*)", re.I)
+PASSWD = re.compile(rb"(pass|userPw|pw)[^(&|=)]*=(?P<pass>[^(&|=|)]*)", re.I)
 
 #pkt = b'POST /signIn.php/user HTTP/1.1\r\nHost: 192.168.0.40\r\nConnection: keep-alive\r\nContent-Length: 23\r\nCache-Control: max-age=0\r\nOrigin: http://192.168.0.40\r\nUpgrade-Insecure-Requests: 1\r\nContent-Type: application/x-www-form-urlencoded\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nReferer: http://192.168.0.40/logIn.php\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: ko-KR,ko;q=0.9,en;q=0.8\r\n\r\nuserId=asdf&userPw=asdf'
-
-def getPkt(pkt):
-	pkt = pkt.decode()
-	return pkt
 
 def obfuscate(passwd):
 	return passwd[0] + "*" * (len(passwd) - 2) + passwd[-1]
 
-def parsePkt(inp):
-	pkt = getPkt(inp)
-
+def parsePkt(pkt):
 	# host parse
 	host = re.search(HOST, pkt)
 	if not host:
