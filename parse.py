d0640a47291cba28495006d2a004914b3e25f9b2
Yang Kyeong Mo <gaeng1107@gmail.com>
2020-02-10 14:44:55 +0900

    Change codes

--- parse.py
+++ parse.py
@@ -11,56 +11,7 @@ CONTYPE = re.compile(rb"content-type\s?:\s?(?P<contenttype> .*)", re.I)
 USERNAME = re.compile(rb"(os_id|userid|login|m_id|id|user_id)=(?P<username>[^(&|=)]*)", re.I)
 PASSWD = re.compile(rb"(pass|userpw|pw|user_pw)[^(&|=)]*=(?P<pass>[^(&|=|\'')]*)", re.I)
 
-pkt = b'POST /signIn.php/user HTTP/1.1\r\nHost: 192.168.0.40\r\nConnection: keep-alive\r\nContent-Length: 23\r\nCache-Control: max-age=0\r\nOrigin: http://192.168.0.40\r\nUpgrade-Insecure-Requests: 1\r\nContent-Type: application/x-www-form-urlencoded\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nReferer: http://192.168.0.40/logIn.php\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: ko-KR,ko;q=0.9,en;q=0.8\r\n\r\nuserId=asdf&userPw=asdf'
-
-def obfuscate(passwd):
-	print(passwd)
-	return passwd[0] + "*" * (len(passwd) - 2) + passwd[-1]
-
-def parsePkt(pkt):
-	# host parse
-	host = re.search(HOST, pkt)
-	if not host:
-		return None
-	host = host.groups()[0]
-	host = host.strip()
-	print (host)
-
-	# method call
-	method = re.search(METHOD, pkt)
-	if not method:
-		return None
-	method = method.groups()[0]
-	print(method)
-
-	# get | post 
-	if method == b'GET':
-		userid = re.search(USERNAME, pkt)
-		if not userid:
-			return None
-		userid = userid.groups()[1]
-		print(userid)
-
-		userpw = re.search(PASSWD, pkt)
-		if not userpw:
-			return None
-		userpw = userpw.groups()[1]
-		print(userpw)
-
-	else:
-		userid = re.findall(USERNAME, pkt)
-		if not userid:
-			return None
-		userid = userid[-1][-1]
-		print(userid)
-		
-		userpw = re.findall(PASSWD, pkt)
-		if not userpw:
-			return None
-		userpw = userpw[-1][-1]
-		print(userpw)
-
-	return (userid, obfuscate(userpw), host)
+pkt = b'POST /signIn.php/user HTTP/1.1\r\nHost: 192.168.0.40\r\nConnection: keep-alive\r\nContent-Length: 23\r\nCache-Control: max-age=0\r\nOrigin: http://192.168.0.40\r\nUpgrade-Insecure-Requests: 1\r\nContent-Type: application/x-www-form-urlencoded\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nReferer: http://192.168.0.40/logIn.php\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: ko-KR,ko;q=0.9,en;q=0.8\r\n\r\nuserId=asdf&userPw=1234'
 
 def insertInfo(conn, cur, id, pw, ip, host):
 	querry = 'INSERT into wos(id, pw, host, ip) values(%s, %s, %s, %s)'
@@ -77,6 +28,53 @@ def cntHost(conn, cur, host):
 	res = cur.fetchall()
 	print(res)
 
+def obfuscate(passwd):
+	return passwd[0] + "*" * (len(passwd) - 2) + passwd[-1]
+
+def parsePkt(pkt):
+	# host parse
+	host = re.search(HOST, pkt)
+	if not host:
+		return None
+	host = host.groups()[0]
+	host = host.decode()
+	print (host)
+
+	# method call
+	method = re.search(METHOD, pkt)
+	if not method:
+		return None
+	method = method.groups()[0]
+
+	# get
+	if method == b'GET':
+		userid = re.search(USERNAME, pkt)
+		if not userid:
+			return None
+		userid = userid.groups()[1]
+		print(userid)
+
+		userpw = re.search(PASSWD, pkt)
+		if not userpw:
+			return None
+		userpw = userpw.groups()[1]
+		print(userpw)
+	# post => last value
+	else:
+		userid = re.findall(USERNAME, pkt)
+		if not userid:
+			return None
+		#userid = userid[-1][-1]
+		print(userid)
+		
+		userpw = re.findall(PASSWD, pkt)
+		if not userpw:
+			return None
+		#userpw = userpw[-1][-1]
+		print(userpw)
+
+	#return (userid, obfuscate(userpw), host)
+	return (userid, userpw, host)
 
 def main():
 	conn = pymysql.connect(host='localhost', user='jyp', password='wldbs11', db='wallofsheep', charset='utf8')
