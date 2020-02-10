1728f17d859ef5a645bd22d44f78dc931695b235
Yang Kyeong Mo <gaeng1107@gmail.com>
2020-02-10 14:47:03 +0900

    Change codes

--- parse.py
+++ parse.py
@@ -52,29 +52,33 @@ def parsePkt(pkt):
 		if not userid:
 			return None
 		userid = userid.groups()[1]
-		print(userid)
+		userid = userid.decode()
+		#print(userid)
 
 		userpw = re.search(PASSWD, pkt)
 		if not userpw:
 			return None
 		userpw = userpw.groups()[1]
-		print(userpw)
+		userid = userid.decode()
+		#print(userpw)
 	# post => last value
 	else:
 		userid = re.findall(USERNAME, pkt)
 		if not userid:
 			return None
-		#userid = userid[-1][-1]
-		print(userid)
+		userid = userid[-1][-1]
+		userid = userid.decode()
+		#print(userid)
 		
 		userpw = re.findall(PASSWD, pkt)
 		if not userpw:
 			return None
-		#userpw = userpw[-1][-1]
-		print(userpw)
+		userpw = userpw[-1][-1]
+		userid = userid.decode()
+		#print(userpw)
 
-	#return (userid, obfuscate(userpw), host)
-	return (userid, userpw, host)
+	return (userid, obfuscate(userpw), host)
+	#return (userid, userpw, host)
 
 def main():
 	conn = pymysql.connect(host='localhost', user='jyp', password='wldbs11', db='wallofsheep', charset='utf8')
@@ -86,9 +90,9 @@ def main():
 		if rlt is not None:
 			uid, upw, host = rlt[0], rlt[1], rlt[2]
 			try:
-				#insertInfo(conn, cur, uid, upw, ip, host)
-				#cntHost(conn, cur, host)
-				print(rlt,ip)
+				insertInfo(conn, cur, uid, upw, ip, host)
+				cntHost(conn, cur, host)
+				#print(rlt,ip)
 			finally:
 				conn.close()
 
