75e4903ed31db50be8ad78f28b904b31a502e604
g4eng <gaeng1107@gmail.com>
2020-02-13 19:56:03 +0900

    Change codes

--- sniff.py
+++ sniff.py
@@ -1,51 +1,15 @@
 import pcap
-import struct
+import dpkt
 
-eth_type_dict = {
-    0x0800 : 'IP',
-    0x0806 : 'ARP'
-}
-
-ip_proto_dict = {
-    0x01 : 'ICMP',
-    0x06 : 'TCP',
-    0x11 : 'UDP'
-}
-
-def mac2str(mac):
-    return ':'.join(map('{:02X}'.format, mac))
-
-def ip2str(ip):
-    return '.'.join(map('{:d}'.format, ip))
-
-def u_ethernet(packet):
-    return struct.unpack('! 6s 6s H', packet)
-
-def u_ip(packet):
-    return struct.unpack('!B B H H H B B H 4s 4s', packet)
-
-def u_tcp(packet):
-    return struct.unpack('!H H 4s 4s B B H H H', packet)
-
-def sniff():
-    sniffer = pcap.pcap(name=None, promisc=True, immediate=True, timeout_ms=50)
-
-    for ts, pkt in sniffer:    
-        dst_mac, src_mac, eth_type = u_ethernet(pkt[:14])            
+def sniff(network_interface):
+    sniffer = pcap.pcap(name=network_interface, promisc=True, immediate=True, timeout_ms=50)
+    for ts, pkt in sniffer:
         try:
-            if eth_type_dict[eth_type] is 'IP':            
-                vhl, tos, tlen, identification, ff, ttl, proto, cs, src_ip, dst_ip = u_ip(pkt[14:34])                        
-                try:
-                    if ip_proto_dict[proto] is 'TCP':                    
-                        src_port, dst_port, seq, ack, tcp_len, *sth = u_tcp(pkt[34:54])
-                        if dst_port is 80:
-                            #print("http packet")
-                            tmplen = (14 + (vhl&0x0F)*4 + (tcp_len>>4)*4)
-                            if tmplen is not len(pkt):
-                                print(pkt[tmplen:])
-                                return pkt[tmplen:], ip2str(src_ip), mac2str(src_mac), 'HTTP'
-
-                except KeyError:                
-                    pass
-        except KeyError:        
+            rdot = dpkt.radiotap.Radiotap(pkt)
+            logical_link_control = dpkt.llc.LLC(rdot.data.data_frame.data)
+            if logical_link_control.data.data.dport is 80 or logical_link_control.data.data.sport is 80:
+                return logical_link_control.data.data
+        except Exception:
             pass
+
+#sniff("wlan0")
