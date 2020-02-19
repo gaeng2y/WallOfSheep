5f08a07c033a0395931fdc8c78aab131e37ab114
Revlr <jjh8501@gmail.com>
2020-02-20 00:20:08 +0900

    Change codes

--- python/capbc.py
+++ python/capbc.py
@@ -8,15 +8,19 @@ import sys
 def mac2str(mac):
     return ':'.join(map('{:02X}'.format, mac))
 
-def sniff(network_interface):
+def sniff(network_interface, conn, cur):
     sniffer = pcap.pcap(name=network_interface, promisc=True, immediate=True, timeout_ms=50)
     for ts, pkt in sniffer:
         try:
             rdot = dpkt.radiotap.Radiotap(pkt)
             ap = rdot.data.ssid.info.decode()
             if ap is not None:
+            	aplist.aplist(conn, cur, mac2str(rdot.data.mgmt.bssid), ap)
             	print(ap + " "*(30-len(ap)) + mac2str(rdot.data.mgmt.bssid))
         except Exception:
             pass
 
-sniff(sys.argv[1])
+
+conn = pymysql.connect(host='localhost', user='jyp', password='wldbs11', db='wallofsheep', charset='utf8')
+cur = conn.cursor()
+sniff(sys.argv[1], conn, cur)
